3fec7a858b5a76ebe348679b22083b1401add6ca
sahil <es9876090372@gmail.com>
2020-08-08 15:07:15 +0530

    first commit

---
+++ esale/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for esale project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esale.settings')
+
+application = get_wsgi_application()
